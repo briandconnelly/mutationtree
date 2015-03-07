f2fe0f0c0485768473d11bc275a8fe37f02eb065
Brian Connelly <bdc@bconnelly.net>
2015-03-07 07:48:34 -0800

    Changed base_fitness parameter name.

--- R/create_population.R
+++ R/create_population.R
@@ -1,10 +1,23 @@
-create_population <- function(size, fitness)
+#' Create a mutationtree population
+#'
+#' \code{create_population} creates a mutationtree population. The population is
+#' started with one genotype at the root of the tree with \code{size}
+#' individuals and fitness \code{base_fitness}.
+#'
+#' @param size The size of the population
+#' @param base_fitness The base fitness of genotypes in the population
+#' @return A mutationtree population
+#'
+#' @export
+#'
+create_population <- function(size, base_fitness)
 {
     assertthat::assert_that(size > 0)
     assertthat::assert_that(fitness > 0)
 
     pop <- igraph::graph.empty(directed=TRUE) +
         igraph::vertex(first_seen=NA, last_seen=NA, level=0, abundance=size,
-                       fitness=fitness,  num_beneficial=0, num_deleterious=0)
+                       effect_size=0, fitness=base_fitness,
+                       num_beneficial=0, num_deleterious=0)
     return(pop)
 }
